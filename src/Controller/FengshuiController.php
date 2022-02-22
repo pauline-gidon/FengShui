494ba6ceb588efbdc79a3c4cd09e8123ad71ecad
pauline gidon <pauline.gidon@brassart.fr>
2022-02-22 17:44:25 +0100

    page static

--- src/Controller/FengshuiController.php
+++ src/Controller/FengshuiController.php
@@ -9,11 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class FengshuiController extends AbstractController
 {
     #[Route('/feng-shui', name: 'fengshui')]
-    public function index(): Response
+    public function fengshui(): Response
     {
-        return $this->render('fengshui/index.html.twig', [
-            'controller_name' => 'FengshuiController',
-        ]);
+        return $this->render('fengshui/fengshui.html.twig');
+    }
+    
+    #[Route('/offre', name: 'offre')]
+    public function offre(): Response
+    {
+        return $this->render('fengshui/offre.html.twig');
     }
 
+    #[Route('/a-propos', name: 'aPropos')]
+    public function aPropos(): Response
+    {
+        return $this->render('fengshui/aPropos.html.twig');
+    }
 }
